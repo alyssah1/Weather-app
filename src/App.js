48972eac342f40e8358c108d388664f39e1bd2e8
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-02-02 15:41:09 -0600

    added form group

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Weather from "./components/index";
 import "./App.css";
+import Form from "./components/form";
 
 const API_key="a33e92de84ca0ba505abc69b46f89446";
 
@@ -19,10 +20,13 @@ class App extends Component{
       description: "",
       error: false,
     };
-    this.getWeather();
 
     this.weatherIcon = {
-      Clouds: "fa-cloud"
+      Thunderstorm: "fa-bolt",
+      Rain: "fa-cloud-showers-heavy",
+      Snow: "fa-snowflake",
+      Clear: "fa-cloud-sun",
+      Clouds: "fa-cloud",
     }
   }
 
@@ -31,30 +35,66 @@ class App extends Component{
     return celsius;
   }
 
-  getWeather = async() => {
-    const API_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`);
+  get_WeatherIcon(icons, rangeID){
+    // eslint-disable-next-line default-case
+    switch(true){
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({icon: this.weatherIcon.Thunderstorm});
+        break;
+      // eslint-disable-next-line no-duplicate-case
+      case rangeID >= 500 && rangeID <= 531:
+      this.setState({icon: this.weatherIcon.Rain});
+      break;
+      // eslint-disable-next-line no-duplicate-case
+      case rangeID >= 600 && rangeID <= 622:
+      this.setState({icon: this.weatherIcon.Snow});
+      break;
+      // eslint-disable-next-line no-duplicate-case
+      case rangeID === 800:
+      this.setState({icon: this.weatherIcon.Clear});
+      break;
+      // eslint-disable-next-line no-duplicate-case
+      case rangeID >= 801 && rangeID <= 804:
+      this.setState({icon: this.weatherIcon.Clouds});
+      break;
+      default: 
+      this.setState({icon: this.weatherIcon.Clouds});
+    }
+  }
+
+  getWeather = async(e) => {
+
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+  if(city && country){
+    const API_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
 
     const response = await API_call.json();
 
     console.log(response);
 
     this.setState({
-      city: response.name,
-      country: response.sys.country,
+      city: `${response.name}, ${response.sys.country}`,
       celsius: this.calCelsius(response.main.temp),
       temp_max: this.calCelsius(response.main.temp_max),
       temp_min: this.calCelsius(response.main.temp_min),
       description: response.weather[0].description,
-      icon: this.weatherIcon.Clouds
-
-
     });
+
+    this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
+    }else{
+      this.setState({error: true});
+    }
   };
 
 
   render() {
     return(
     <div className="App">
+      <Form loadweather={this.getWeather} error={this.state.error} />
       <Weather 
       city={this.state.city} 
       country={this.state.country}
--- src/components/index.js
+++ src/components/index.js
@@ -4,12 +4,13 @@ function Weather (props) {
     return (
         <>
         <div className="container">
-        <div className="cards">
-        <h1>{props.city}, {props.country}</h1>
+        <div className="cards pt-4">
+        <h1>{props.city}</h1>
         <h5 className="py-4">
             <i className={`fas ${props.weatherIcon} fa-4x`}></i>
         </h5>
-        <h1 className="py-2">{props.temp_celsius}&deg;</h1>
+        
+        {props.temp_celsius ? (<h1 className="py-2">{props.temp_celsius}&deg;</h1>) :null}
 
         {/* show max and min tempurature */}
         {minmaxTemp(props.temp_min, props.temp_max)}
@@ -22,12 +23,14 @@ function Weather (props) {
 };
 
 function minmaxTemp(min, max){
-    return (
+    if(min&&max){
+        return(
         <h3>
             <span className="px-4">{min}&deg;</span>
             <span className="px-4">{max}&deg;</span>
         </h3>
-    )
+        );
+    }
 }
 
 export default Weather;
